f2e9d073af62f78dc6d8746d2024aec87b05cfb1
dougbradbury <doug@8thlight.com>
2010-12-17 07:41:36 -0600

    ok I'm done now

--- spec/eu/eu_1_spec.clj
+++ spec/eu/eu_1_spec.clj
@@ -35,9 +35,5 @@
   (it "should solve big euler 3"
     (should= 6857 (euler3 600851475143)))
 
-;The prime factors of 13195 are 5, 7, 13 and 29.
-;What is the largest prime factor of the number 600851475143 ?
-
-
   )
 (run-specs)
